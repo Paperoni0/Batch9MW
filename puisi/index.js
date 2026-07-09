6081ddaae01b4b7a8ade37e3451a80a115c1ddb7
Paperoni <111691853+Paperoni0@users.noreply.github.com>
2026-07-09 20:25:13 +0700

    upd

---
+++ puisi/index.js
@@ -0,0 +1,6 @@
+function toggleNav() {
+    const nav = document.querySelector('.class-list');
+    const verticalLine = document.getElementById('navVerticalLine');
+    nav.style.display = nav.style.display === 'none' ? 'block' : 'none';
+    verticalLine.style.display = nav.style.display === 'none' ? 'block' : 'none';
+}
