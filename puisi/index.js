e60a3083cced00a3d7a27b4c9f12c6550933f2e5
Paperoni <111691853+Paperoni0@users.noreply.github.com>
2026-07-11 22:15:56 +0700

    bug fix

--- puisi/index.js
+++ puisi/index.js
@@ -5,5 +5,5 @@ for (let i = 0; i < 40; i++) {
     particle.style.left = Math.random() * 100 + 'vw';
     particle.style.animationDuration = (3 + Math.random() * 5) + 's';
     particle.style.animationDelay = Math.random() * 5 + 's';
-    particleContainer.appendChild(p);
+    particleContainer.appendChild(particle);
 }
