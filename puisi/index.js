20320b184395d3fa6ec5e0087af0da0ac7fcc490
Paperoni <111691853+Paperoni0@users.noreply.github.com>
2026-07-11 15:06:54 +0700

    upd

--- puisi/index.js
+++ puisi/index.js
@@ -4,3 +4,12 @@ function toggleNav() {
     nav.style.display = nav.style.display === 'none' ? 'block' : 'none';
     verticalLine.style.display = nav.style.display === 'none' ? 'block' : 'none';
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.addEventListener('contextmenu', event => event.preventDefault());
+    window.oncontextmenu = (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+        return false;
+    };
+});
