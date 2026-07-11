c0e9f9a1e4f2491821a3c5f0424514d93cdc4cf1
Paperoni <111691853+Paperoni0@users.noreply.github.com>
2026-07-11 22:23:02 +0700

    upd

--- main/js/publicNav.js
+++ main/js/publicNav.js
@@ -1,6 +1,7 @@
-function toggleNav() {
-        const nav = document.querySelector('.class-list');
-        const verticalLine = document.getElementById('navVerticalLine');
-        nav.style.display = nav.style.display === 'none' ? 'block' : 'none';
-        verticalLine.style.display = nav.style.display === 'none' ? 'block' : 'none';
-      }
+const fab = document.querySelector('.fab');
+fab.addEventListener('click', () => {
+  const nav = document.querySelector('.class-list');
+  const verticalLine = document.getElementById('navVerticalLine');
+  nav.style.display = nav.style.display === 'none' ? 'block' : 'none';
+  verticalLine.style.display = nav.style.display === 'none' ? 'block' : 'none';
+});
