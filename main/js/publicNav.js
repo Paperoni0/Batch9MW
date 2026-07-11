8fd42d67c316686b333c6a0a476e6eafcc53cc6d
Paperoni <111691853+Paperoni0@users.noreply.github.com>
2026-07-11 22:37:53 +0700

    upd

--- main/js/publicNav.js
+++ main/js/publicNav.js
@@ -2,6 +2,6 @@ const fab = document.querySelector('.fab');
 fab.addEventListener('click', () => {
   const nav = document.querySelector('.class-list');
   const verticalLine = document.getElementById('navVerticalLine');
-  nav.style.display = nav.style.display === 'none' ? 'block' : 'none';
+  nav.style.display = nav.style.display === 'none' ? 'flex' : 'none';
   verticalLine.style.display = nav.style.display === 'none' ? 'block' : 'none';
 });
