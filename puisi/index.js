6da90975d3f692cc9435720197627b24f083b1ef
Paperoni <111691853+Paperoni0@users.noreply.github.com>
2026-07-12 08:48:25 +0700

    Update index.js

--- puisi/index.js
+++ puisi/index.js
@@ -3,6 +3,7 @@ for (let i = 0; i < 40; i++) {
     const particle = document.createElement('div');
     particle.className = 'particle';
     particle.style.left = Math.random() * 100 + 'vw';
+    particle.style.top = '100%';
     particle.style.animationDuration = (3 + Math.random() * 5) + 's';
     particle.style.animationDelay = Math.random() * 5 + 's';
     particleContainer.appendChild(particle);
